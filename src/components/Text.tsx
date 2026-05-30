d33444681ea0182e9a33ac13459d9f37cd86f2e5
shamonesmail <shamonesmail@ziyaacademy.co.in>
2026-05-30 10:39:36 +0530

    folder test

---
+++ src/components/Text.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Text: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Text
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const HomePage: React.Fc = () => {
+  return <div>hi</div>
+}
+
+export default HomePage
